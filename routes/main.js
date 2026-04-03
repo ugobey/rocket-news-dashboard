887a087f7e45619ccb1ae7c65685ca9dbf75e19e
Uri Gobey <uri.gobey@contentsquare.com>
2026-04-03 12:44:53 +0300

    Update main.js

--- routes/main.js
+++ routes/main.js
@@ -11,20 +11,53 @@ require("dotenv").config();
 const pkg = require("../package.json");
 const pikudHaorefCitiesJSON = require("pikud-haoref-api/cities.json");
 
-router.use("/", async function (req, res) {
+function logError(context, err) {
+    console.error(`${service_name} ERROR [${context}]`, {
+        message: err?.message,
+        stack: err?.stack,
+    });
+}
+
+function sendInternalError(res, err, context) {
+    logError(context, err);
+
+    if (res.headersSent) {
+        return;
+    }
+
+    res.status(500).send("Internal Server Error");
+}
+
+function asyncHandler(fn, context) {
+    return async function wrappedAsyncHandler(req, res, next) {
+        try {
+            await fn(req, res, next);
+        } catch (err) {
+            sendInternalError(res, err, context);
+        }
+    };
+}
+
+function buildMainViewModel() {
     try {
-        res.render(service_page_name, {
+        return {
             title: service_name,
             appVersion: pkg.version,
             cities: JSON.stringify(pikudHaorefCitiesJSON),
             googleMapsApiKey: config.googleMapsApiKey,
-        });
+        };
     } catch (err) {
-        console.log(service_name + " ERROR", err);
-        res.statusCode = 200;
-        res.write("ERROR");
-        res.end();
+        logError("buildMainViewModel", err);
+        throw err;
     }
-});
+}
+
+router.use(
+    "/",
+    asyncHandler(async function mainRouteHandler(req, res) {
+        const viewModel = buildMainViewModel();
+        res.render(service_page_name, viewModel);
+    }, "mainRouteHandler"),
+);
 
 module.exports = router;
